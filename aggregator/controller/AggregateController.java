82e125b10d52ecc3c8460996b8b9e0a30cd98d24
Rakesh <rakesh.das@mindfiresolutions.com>
2025-05-03 12:39:00 +0530

    Task #2 - complete

--- aggregator/controller/AggregateController.java
+++ aggregator/controller/AggregateController.java
@@ -1,20 +1,27 @@
 package aggregator.controller;
 
+import aggregator.response.Transaction;
+import aggregator.service.TransactionsGetterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/aggregate")
 public class AggregateController {
 
+    @Autowired
+    TransactionsGetterService transactionsGetterService;
+
     @GetMapping
-    public String getText() {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8889/ping";
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-        return response.getBody();
+    public List<Transaction> getTransactions(@RequestParam("account") String account) {
+        return transactionsGetterService.getTransactions(account);
     }
+
 }
